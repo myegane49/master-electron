a6038e2ac058e7b90e2e1f7d7228a14f4c9fc9ee
Mohammad Ahmady Yeganeh <myegane49@protonmail.com>
2021-04-18 20:05:14 +0430

    Adds the functionality for the add button and the modal and the ipc between renderer and main

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 // Modules
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
 const windowStateKeeper = require('electron-window-state')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+
+ipcMain.on('new-item', (event, itemUrl) => {
+  setTimeout(() => {
+    event.sender.send('new-item-success', 'new item from main process')
+  }, 2000);
+})
 
 function createWindow () {
   const state = windowStateKeeper({
--- renderer/app.js
+++ renderer/app.js
@@ -1,3 +1,48 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const {ipcRenderer} = require('electron')
+
+const showModal = document.getElementById('show-modal')
+const closeModal = document.getElementById('close-modal')
+const modal = document.getElementById('modal')
+const addItem = document.getElementById('add-item')
+const itemUrl = document.getElementById('url')
+
+const toggleModalButtons = () => {
+  if (addItem.disabled) {
+    addItem.disabled = false
+    addItem.style.opacity = 1
+    addItem.innerText = 'Add Item'
+    closeModal.style.display = 'inline'
+  } else {
+    addItem.disabled = true
+    addItem.style.opacity = .5
+    addItem.innerText = 'Adding...'
+    closeModal.style.display = 'none'
+  }
+}
+
+showModal.addEventListener('click', event => {
+  modal.style.display = 'flex'
+  itemUrl.focus()
+})
+
+closeModal.addEventListener('click', event => {
+  modal.style.display = 'none'
+})
+
+addItem.addEventListener('click', event => {
+  if (itemUrl.value) {
+    ipcRenderer.send('new-item', itemUrl.value)
+    toggleModalButtons()
+  }
+})
+
+itemUrl.addEventListener('keyup', event => {
+  if (event.key === 'Enter') addItem.click()
+})
+
+ipcRenderer.on('new-item-success', (event, newItem) => {
+  console.log(newItem)
+  toggleModalButtons()
+  modal.style.display = 'none'
+  itemUrl.value = ''
+})
